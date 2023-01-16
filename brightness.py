001974c110f380131b0def330e46851c468fabba
Apples1243 <110793635+Apples1243@users.noreply.github.com>
2023-01-16 14:21:21 +0900

    Update brightness.py

--- brightness.py
+++ brightness.py
@@ -6,6 +6,9 @@ src = cv2.imread('lenna.bmp', cv2.IMREAD_GRAYSCALE)
 
 
 # destination
+# dst1 = src + 100    # <----- 픽셀값이 255보다 큰 경우 오류 발생
+# dst2 = src - 100    # <----- 픽셀값이 0보다 작은 경우 오류 발생
+
 dst1 = cv2.add(src, 100)     # 더하기 (밝게)
 dst2 = cv2.subtract(src, 100)    # 빼기 (어둡게)
 
