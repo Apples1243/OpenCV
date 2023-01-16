ed898858cc04e8d41d51b20bd90ec2690dc7789a
Apples1243 <110793635+Apples1243@users.noreply.github.com>
2023-01-16 13:30:37 +0900

    Update matrix2.py

--- matrix2.py
+++ matrix2.py
@@ -2,10 +2,13 @@ import cv2
 
 img1 = cv2.imread('lenna.bmp', cv2.IMREAD_COLOR)
 
-img2 = img1[200:400, 200:400, :]
+img2 = img1[200:400, 200:400, :]    # 공유
+img3 = img1[200:400, 200:400, :].copy()     # 복사
 
+img2 = img2 + 50    # 모든 픽셀값을 50만큼 증가
 
 cv2.imshow('girl', img1)
-cv2.imshow('face', img2)
+cv2.imshow('face #1', img2)
+cv2.imshow('face #2', img3)
 cv2.waitKey()
 cv2.destroyAllWindows()
